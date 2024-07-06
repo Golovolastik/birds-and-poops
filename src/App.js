46bf2f851e9cb1296e9b87547859710b9169f955
Golovolastik <yankovich.aliaksei@gmail.com>
2024-07-06 19:03:57 +0300

    feature: click button function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Header/>
         <Points/>
         <TimerBar initialTime={60}/>
-        <ClaimButton onClick={onClose}></ClaimButton>
+        <ClaimButton></ClaimButton>
         <FooterMenu/>
     </div>
   );
