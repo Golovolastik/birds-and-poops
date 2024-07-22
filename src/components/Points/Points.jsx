c8ca7242f0e14205c3f2f7907e3b849eeb73837c
Golovolastik <yankovich.aliaksei@gmail.com>
2024-07-22 15:57:28 +0300

    feature: try to get uid

--- src/components/Points/Points.jsx
+++ src/components/Points/Points.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import './Points.css';
 
 const Points = () => {
+    const { user } = useTelegram();
+    const [points, setPoints] = useState(null);
+
+    useEffect(() => {
+        const fetchPoints = async () => {
+            try {
+                const response = await fetch(`https://potty-pals.fun/api/points`, {
+                    headers: {
+                        user: user?.id,
+                    }
+                });
+                const data = await response.json();
+                setPoints(data.points);
+            } catch (error) {
+                console.error('Error fetching points:', error);
+            }
+        };
+
+        if (user?.id) {
+            fetchPoints();
+        }
+    }, [user]);
+
     return (
-        <div>
-            100000
+        <div className="points">
+            {points !== null ? (
+                <span>Your points: {points}</span>
+            ) : (
+                <span>Loading points...</span>
+            )}
         </div>
     );
 };
