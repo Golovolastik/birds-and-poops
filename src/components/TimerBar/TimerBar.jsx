b0078fd11288df9ac44e741f84ab53d8cc71db2c
Golovolastik <yankovich.aliaksei@gmail.com>
2024-07-26 20:18:26 +0300

    feature: timer animation

--- src/components/TimerBar/TimerBar.jsx
+++ src/components/TimerBar/TimerBar.jsx
@@ -3,10 +3,12 @@ import './TimerBar.css';
 import {useTelegram} from "../../hooks/useTelegram";
 
 const TimerBar = ({ setStatus }) => {
-    const { user } = useTelegram();
+    const {user} = useTelegram();
     const [timer, setTimer] = useState('Loading');
 
-    useEffect( () => {
+    useEffect(() => {
+        let timerFunction;
+
         const fetchTimer = async () => {
             try {
                 const response = await fetch('https://potty-pals.fun/api/last-claim-time', {
@@ -18,42 +20,34 @@ const TimerBar = ({ setStatus }) => {
                 const data = await response.json();
                 const lastClaimTime = Date.parse(data.data);
                 setStatus('Running');
-                const timerFunction = setInterval(function () {
+
+                timerFunction = setInterval(() => {
                     const timeFromClaim = new Date().getTime() - lastClaimTime;
                     const hours = Math.floor((timeFromClaim % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 3;
                     const minutes = Math.floor((timeFromClaim % (1000 * 60 * 60)) / (1000 * 60));
                     const seconds = Math.floor((timeFromClaim % (1000 * 60)) / 1000);
+
                     if (hours >= 8) {
                         setStatus('Ready');
                         setTimer('Ready');
                         clearInterval(timerFunction);
                     } else {
-                        let timer = (7-hours) + ':' + (59-minutes) + ':' + (59-seconds);
-                        setTimer(timer);
+                        const formattedTimer = `${7 - hours}:${59 - minutes}:${59 - seconds}`;
+                        setTimer(formattedTimer);
                     }
                 }, 1000);
-
             } catch (error) {
                 console.error('Error fetching time: ', error);
             }
-        }
+        };
+
         if (user?.id) {
             fetchTimer();
         }
+
+        // Cleanup interval on component unmount or user change
+        return () => clearInterval(timerFunction);
+
     }, [user, setStatus]);
-
-    return (
-        <div className={'timer'}>
-            {timer}
-        </div>
-        // <div>
-        //     {time > 0 ? (
-        //         <div className={'timer'}>{Math.floor(time / 60)} minutes {Math.floor(time % 60)} seconds remaining</div>
-        //     ) : (
-        //         <div className={'timer'}>Ready to claim</div>
-        //     )}
-        // </div>
-    );
-};
-
+}
 export default TimerBar;
