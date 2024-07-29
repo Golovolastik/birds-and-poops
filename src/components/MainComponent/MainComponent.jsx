ecc5eb7920323f659926e0fee96917b1065d6555
Golovolastik <yankovich.aliaksei@gmail.com>
2024-07-30 01:09:14 +0300

    feature: timer animation

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -62,7 +62,7 @@ const MainComponent = () => {
 
     return (
         <div className={'container'}>
-            <Points points={points}/>
+            <Points userPoints={points}/>
             <span className={'userId'}>
                 Hi! {user?.username}
             </span>
