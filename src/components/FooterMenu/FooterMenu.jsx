ed4d045ab1964a2d41047c756d470f49c2a1866f
Golovolastik <yankovich.aliaksei@gmail.com>
2024-08-02 23:03:05 +0300

    feature: new ui

--- src/components/FooterMenu/FooterMenu.jsx
+++ src/components/FooterMenu/FooterMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './FooterMenu.css'
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const FooterMenu = () => {
     return (
