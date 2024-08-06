364bb72abd86e9888969132b6becd286c60a30cf
Golovolastik <yankovich.aliaksei@gmail.com>
2024-08-06 11:33:23 +0300

    feature: new ui

--- src/components/FooterMenu/FooterMenu.jsx
+++ src/components/FooterMenu/FooterMenu.jsx
@@ -6,8 +6,8 @@ const FooterMenu = () => {
     return (
         <div className={'footer-menu'}>
             <div>friends</div>
-            <Link to="/tasks">tasks</Link>
-            <Link to="/">farm</Link>
+            <Link to="/tasks" className={'footer-link'}>tasks</Link>
+            <Link to="/" className={'footer-link'}>farm</Link>
             <div>leaderboard</div>
         </div>
     );
